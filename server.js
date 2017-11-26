2c9cb5bb8d16f91370406f45401aa40ae36308d4
vuhoanglinh96 <linhvh.96@gmail.com>
2017-11-26 22:53:03 +0700

    push heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+console.log(process.env.PORT);
+
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
